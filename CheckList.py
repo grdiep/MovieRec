d5d6bc0b7cf700444c09a7a110024c11ff498f37
Grace Diep <gracediep101@gmail.com>
2023-12-29 21:57:51 -0600

    Created Checklist class

--- CheckList.py
+++ CheckList.py
@@ -1,48 +1,46 @@
 import tkinter as tk
 
-class CheckList:
+class CheckList():
 
-    # create a new class for the tkinter(?) --------------------- 
-    
-window = tk.Tk()
-greeting = tk.Label(text="I'm looking for a show within these genre(s):")
-greeting.pack()
+    list1 = dict()
+    list2 = []
 
-list1 = dict()
+    def __init__(self, genre_dict):
+        self.window = tk.Tk()
+        self.greeting = tk.Label(text="I'm looking for a show within these genre(s):")
+        self.greeting.pack()
 
-for key in genre_dict: # poulate the options for checkbox 
-    var1 = tk.IntVar() # think of as a state that will change depending if checkbox clicked or not
-    genre = genre_dict[key]
-    c1 = tk.Checkbutton(window, text=genre, variable=var1, onvalue=1, offvalue=0).pack(anchor='w')
-    list1[genre] = var1
+        for key in genre_dict: # poulate the options for checkbox 
+            var1 = tk.IntVar() # think of as a state that will change depending if checkbox clicked or not
+            genre = genre_dict[key]
+            c1 = tk.Checkbutton(self.window, text=genre, variable=var1, onvalue=1, offvalue=0).pack(anchor='w')
+            self.list1[genre] = var1
 
-def clearAll():
-    if bool(list1):
-        pass
-        print("list is empty")
-    for item in list1: 
-        list1[item].set(0)
+        self.clearBtn = tk.Button(text="Clear", command=self.clearAll).pack()
+        self.SelectAllBtn = tk.Button(text="Select All", command=self.selectAll).pack()
+        self.submitBtn = tk.Button(text="Submit", command=self.submit).pack()
 
-#clear button 
-clearBtn = tk.Button(text="Clear", command=clearAll).pack()
+        self.window.mainloop() 
 
-def selectAll():
-    for item in list1: 
-        list1[item].set(1)
-    
-#select all button 
-SelectAllBtn = tk.Button(text="Select All", command=selectAll).pack()
+        
+    def clearAll(self):
+        if bool(self.list1):
+            pass
+            print("list is empty")
+        for item in self.list1: 
+            self.list1[item].set(0)
 
-def submit(): 
-    list2 = [] 
-    for item in list1: 
-        if list1[item].get() == 1:
-            list2.append(item)
 
-    print(list2)
+    def selectAll(self):
+        for item in self.list1: 
+            self.list1[item].set(1)
 
-#submit button
-submitBtn = tk.Button(text="Submit", command=submit)
-submitBtn.pack()
+    def submit(self): 
+        for item in self.list1: 
+            if self.list1[item].get() == 1:
+                self.list2.append(item)
+        
+        print(self.list2)
 
-window.mainloop()
+    def __str__(self):
+        print(self.list1)
--- genre.py
+++ genre.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import tkinter as tk 
+import CheckList as ck
 
 
 # API call to TMHB -------------------------------
@@ -19,50 +20,7 @@ for genre in data['genres']:
     genre_dict[id] = genre
     
 
-# create a new class for the tkinter(?) --------------------- 
-    
-window = tk.Tk()
-greeting = tk.Label(text="I'm looking for a show within these genre(s):")
-greeting.pack()
+# TODO:Testing Checklist ------------------------------------
+checkList = ck.CheckList(genre_dict)
 
-list1 = dict()
-
-for key in genre_dict: # poulate the options for checkbox 
-    var1 = tk.IntVar() # think of as a state that will change depending if checkbox clicked or not
-    genre = genre_dict[key]
-    c1 = tk.Checkbutton(window, text=genre, variable=var1, onvalue=1, offvalue=0).pack(anchor='w')
-    list1[genre] = var1
-
-def clearAll():
-    if bool(list1):
-        pass
-        print("list is empty")
-    for item in list1: 
-        list1[item].set(0)
-
-#clear button 
-clearBtn = tk.Button(text="Clear", command=clearAll).pack()
-
-def selectAll():
-    for item in list1: 
-        list1[item].set(1)
-    
-#select all button 
-SelectAllBtn = tk.Button(text="Select All", command=selectAll).pack()
-
-def submit(): 
-    list2 = [] 
-    for item in list1: 
-        if list1[item].get() == 1:
-            list2.append(item)
-
-    print(list2)
-
-#submit button
-submitBtn = tk.Button(text="Submit", command=submit)
-submitBtn.pack()
-
-window.mainloop()
-
-# ------------------------
 
