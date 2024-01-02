649eb96a0a75e80d483e8e4c352b2d46bd0684dd
Grace Diep <gracediep101@gmail.com>
2024-01-01 18:57:32 -0600

    CheckList updated.

--- CheckList.py
+++ CheckList.py
@@ -14,9 +14,9 @@ class CheckList():
             var1 = tk.IntVar() # think of as a state that will change depending if checkbox clicked or not
             genre = genre_dict[key]
             c1 = tk.Checkbutton(self.window, text=genre, variable=var1, onvalue=1, offvalue=0).pack(anchor='w')
-            self.list1[genre] = var1
+            self.list1[genre] = var1 #to check if checkboxes have been checked off 
 
-        self.clearBtn = tk.Button(text="Clear", command=self.clearAll).pack()
+        self.clearBtn = tk.Button(text="Clear All", command=self.clearAll).pack()
         self.SelectAllBtn = tk.Button(text="Select All", command=self.selectAll).pack()
         self.submitBtn = tk.Button(text="Submit", command=self.submit).pack()
 
@@ -29,7 +29,9 @@ class CheckList():
             print("list is empty")
         for item in self.list1: 
             self.list1[item].set(0)
-
+        
+        self.list2.clear()
+        
 
     def selectAll(self):
         for item in self.list1: 
@@ -37,10 +39,8 @@ class CheckList():
 
     def submit(self): 
         for item in self.list1: 
-            if self.list1[item].get() == 1:
+            if (self.list1[item].get() == 1) & (item not in self.list2):
                 self.list2.append(item)
         
         print(self.list2)
 
-    def __str__(self):
-        print(self.list1)
--- genre.py
+++ genre.py
@@ -3,8 +3,8 @@ import json
 import tkinter as tk 
 import CheckList as ck
 
-
-# API call to TMHB -------------------------------
+voting_avg = 9
+# API call to get genres from TMHB -------------------------------
 
 params = {"api_key": '78ad3cf3bc5f962e42fd7740e8cb33d0'}
 r = requests.get("https://api.themoviedb.org/3/genre/movie/list", params=params)
@@ -18,9 +18,58 @@ for genre in data['genres']:
     id = genre['id']
     genre = genre['name']
     genre_dict[id] = genre
-    
 
-# TODO:Testing Checklist ------------------------------------
+# print(genre_dict)    
+
+# converting genre name to its ID --------------------------------------
+
+def genreID_conversion(list):
+    # take the list 2 which contains our genre of interest 
+    # and compare it to our genre_dict 
+    # convert genre name >> genre_ID 
+    genreID_list = []
+    for genre in checkList.list2:
+        for genre_ID in genre_dict:
+            if genre_dict[genre_ID] == genre:
+                genreID_list.append(genre_ID)
+                break
+
+    return genreID_list
+
+
+# calling Checklist class ------------------------------------
+
 checkList = ck.CheckList(genre_dict)
+genreID_list = genreID_conversion(checkList.list2)
+# print(genreID_list)
+
+
+#TODO: API call to discover API  -----------------------------------
+
+params = {"api_key": '78ad3cf3bc5f962e42fd7740e8cb33d0', 
+          "sort_by": 'vote_average.asc',
+          "vote_average.gte": voting_avg,
+          "with_genres": genreID_list}
+
+r = requests.get("https://api.themoviedb.org/3/discover/movie", params=params)
+data = r.json()
+
+for item in data['results']:
+    movieTitle = item['original_title']
+    print(movieTitle) 
+
+    overview = item['overview']
+    print(overview)
+
+    url = item['poster_path']
+    print(url)
+
+
+# ---------------------------------------
+
+
+
+
+
 
 
